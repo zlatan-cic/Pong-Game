5d4bb82dc94bc97fffe6e203d19aed6d29a463d3
zlatan-cic <smajicz999@gmail.com>
2025-05-20 08:17:45 +0200

    Buggy Pong game – prep for bug review

--- Bat.cpp
+++ Bat.cpp
@@ -1,49 +1,44 @@
 #include "Bat.h"
 
-//This is a constructor and it is called
+// This the constructor and it is called 
 // when we create an object
-
 Bat::Bat(float startX, float startY) : m_Position(startX, startY)
 {
-	m_Shape.setSize(sf::Vector2f(50, 5));
-	m_Shape.setPosition(m_Position);
+    m_Shape.setSize(sf::Vector2f(150, 50));
+    m_Shape.setPosition(m_Position);
 }
 
 FloatRect Bat::getPosition()
 {
-	return m_Shape.getGlobalBounds();
+    return m_Shape.getGlobalBounds();
 }
 RectangleShape Bat::getShape()
 {
-	return	m_Shape;
+    return m_Shape;
 }
 void Bat::moveLeft()
 {
-	m_MovingLeft = true;
+    m_MovingLeft = true;
 }
 void Bat::moveRight()
 {
-	m_MovingRight = true;
+    m_MovingRight = true;
 }
 void Bat::stopLeft()
 {
-	m_MovingLeft = false;
+    m_MovingLeft = false;
 }
 void Bat::stopRight()
 {
-	m_MovingRight = false;
+    m_MovingRight = false;
 }
-
 void Bat::update(Time dt)
 {
-	if (m_MovingLeft)
-	{
-		m_Position.x -= m_Speed * dt.asSeconds();
-	}
-	if (m_MovingRight)
-	{
-		m_Position.x += m_Speed * dt.asSeconds();
-	}
-
-	m_Shape.setPosition(m_Position);
+    if (m_MovingLeft) {
+        m_Position.x -= m_Speed * dt.asSeconds();
+    }
+    if (m_MovingRight) {
+        m_Position.x += m_Speed * dt.asSeconds();
+    }
+    m_Shape.setPosition(m_Position);
 }
--- main.cpp
+++ main.cpp
@@ -2,99 +2,133 @@
 #include <sstream>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
-
+#include "Ball.h"
 int main()
 {
-	// Create a Video mode object
-	VideoMode vm(1920, 1080);
+    // Create a video mode object
+    VideoMode vm(1920, 1080);
+    // Create and open a window for the game
+    RenderWindow window(vm, "Pong", Style::Fullscreen);
+    int score = 0;
+    int lives = 3;
 
-	// Create and Open a window for game
-	RenderWindow window(vm, "Pong Game!!", Style::Fullscreen);
-	int score = 0;
-	int lives = 3;
-
-	// Create a bat at the bottom center of the screen;
-	Bat bat(1920 / 2, 1080 - 20);
-	//
-	// Create a text object called HUD
-	Text hud;
-	Font font;
-	font.loadFromFile("fonts/Gameplay.ttf");
-	// Set the font to our retro-style
-	hud.setFont(font);
-	// make it nice and big size
-	hud.setCharacterSize(75);
-	//Chose color for font
-	hud.setFillColor(Color::White);
-	hud.setPosition(20, 20);
-
-
-	// Here is our clock for timing everything
-	Clock clock;
-
-	while (window.isOpen())
-	{
-		//****************************
-		//Handle the player input
-		//****************************
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				// Quit the game when the window is closed
-				window.close();
-		}
-		// Handle the Player quiting
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
-			window.close();
-		}
-
-		// Handle the pressing and relising the arrow kkey for left and right
-		// Left arrow key
-		if (Keyboard::isKeyPressed(Keyboard::Left))
-		{
-			bat.moveLeft();
-		}
-		else
-		{
-			bat.stopLeft();
-		}
-		// Right arrow key
-		if (Keyboard::isKeyPressed(Keyboard::Right))
-		{
-			bat.moveRight();
-		}
-		else
-		{
-			bat.stopRight();
-		}
+    // Create a bat at the bottom center of the screen
+    Bat bat(1920 / 2, 1080 - 20);
+    // We will add a ball in the next chapter
+    Ball ball(1920 / 2, 0);
+    // Create a Text object called HUD
+    Text hud;
+    // A cool retro-style font
+    Font font;
+    font.loadFromFile("fonts/Gameplay.ttf");
+    // Set the font to our retro-style
+    hud.setFont(font);
+    // Make it nice and big
+    hud.setCharacterSize(75);
+    // Choose a color
+    hud.setFillColor(Color::White);
+    hud.setPosition(20, 20);
+    // Here is our clock for timing everything
+    Clock clock;
+    while (window.isOpen())
+    {
+        /*
+        Handle the player input
+        ****************************
+        ****************************
+        ****************************
+        */
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                // Quit the game when the window is closed
+                window.close();
+        }
+        // Handle the player quitting
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        // Handle the pressing and releasing of the arrow keys
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            bat.moveLeft();
+        }
+        else
+        {
+            bat.stopLeft();
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            bat.moveRight();
+        }
+        else
+        {
+            bat.stopRight();
+        }
 
 
+        /*
+        Update the bat, the ball and the HUD
+        *****************************
+        *****************************
+        *****************************
+        */
+        // Update the delta time
+        Time dt = clock.restart();
+        bat.update(dt);
+        ball.update(dt);
+        // Update the HUD text
+        std::stringstream ss;
+        ss << "Score:" << score << "  Lives:" << lives;
+        hud.setString(ss.str());
 
-		//*****************************
-		//Update the bat, the ball and the HUD
-		//*****************************
-		// Update the delta time
-		Time dt = clock.restart();
-		bat.update(dt);
+        // Handle ball hitting the bottom
+        if (ball.getPosition().top > window.getSize().y)
+        {
+            // reverse the ball direction
+            ball.reboundBottom();
+            // Remove a life
+            lives--;
+            // Check for zero lives
+            if (lives < 1) {
+                // reset the score
+                score = 0;
+                // reset the lives
+                lives = 3;
+            }
+        }
 
-		// Update the HUD text
-		std::stringstream ss;
-		ss << "Score: " << score << " Lives: " << lives;
-		hud.setString(ss.str());
+        // Handle ball hitting sides
+        if (ball.getPosition().left < 0 ||
+            ball.getPosition().left + ball.getPosition().width> window.getSize().x)
+        {
+            ball.reboundSides();
+        }
+
+        // Has the ball hit the bat?
+        if (ball.getPosition().intersects(bat.getPosition()))
+        {
+            // Hit detected so reverse the ball and score a point
+            ball.reboundBatOrTop();
+        }
+
+
+        /*
+        Draw the bat, the ball and the HUD
+        *****************************
+        *****************************
+        *****************************
+        */
+        window.clear();
+        window.draw(hud);
+        window.draw(bat.getShape());
+        window.draw(ball.getShape());
+        window.display();
 
 
 
-		//*****************************
-		//Draw the bat, the ball and the HUD
-		//*****************************
-		window.clear();
-		window.draw(hud);
-		window.draw(bat.getShape());
-		window.display();
-
-	}
-
-	return 0;
+    }
+    return 0;
 }
